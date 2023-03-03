c612583332239e21cf1976706b5e6bcbf242c90b
joaodedeusrsfilho <joaodedeusrsfilho@gmail.com>
2023-03-03 09:55:23 -0300

    add file

---
+++ CuriosidadeInt.java
@@ -0,0 +1,17 @@
+package com.junior.curso_java_basico.aula11;
+
+public class CuriosidadeInt {
+
+	public static void main(String[] args) {
+		
+		int var1 = 2147483647;
+		int var2 = 1;
+		
+		System.out.println(var1+var2);
+		//os numeros no java funcionam como uma roleta no momento
+		//que alcança o valor maximo ele volta para os numeros
+		//negativos
+
+	}
+
+}
